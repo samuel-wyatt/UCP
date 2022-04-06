3d87ec2ac8528c57f2f5257166d08ccaa4369a0e
samuel-wyatt <samuel.wyatt2@outlook.com>
2022-04-06 20:11:38 +0800

    Last part of question 7 to complete

--- Worksheets and Lectures/Week04/cmdLine.c
+++ Worksheets and Lectures/Week04/cmdLine.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include "question2.h"
-#define MAXLENGTH 10
+#include "functions.h"
 
 int main(int args, char* argv[]) {
 
-    int i, count = 0, length;
-    int array[MAXLENGTH];
-    char *argument = argv[1];
+    int i, count = 0; 
+    int length = atoi(argv[2]);
 
-    if (args < 3) {
+    char *argument = argv[1];
+    int *array = (int*)malloc(length*sizeof(1));
+
+    do {
+        for (i = 0; i < args; i++) {
+        argv[i] = argv[i + 1];
+        }
+        count += 1;
+    } while (count != 3);
+
+    if (args < 4) {
         printf("ERROR: No/not enough argument(s) were provided.\n");
 
     } else {
-        do {
-
-            for (i = 0; i < args; i++) {
-            argv[i] = argv[i + 1];
-            }
-            count += 1;
-        } while (count != 2);
-        length = args - 2;
-
         strToInt(argv, array, length);
+        lowerUpper(argument);
 
-        if (strcmp(argument, "sum") == 0) {
-            printf("%d\n", sum(array, length));
+        if (strcmp(argument, "SUM") == 0) {
+            printf("Sum = %d\n", sum(array, length));
 
-        } else if (strcmp(argument, "max") == 0) {
-            printf("%d\n", max(array, length));
+        } else if (strcmp(argument, "MAX") == 0) {
+            printf("Index of largest element = %d\n", max(array, length));
 
-        } else if (strcmp(argument, "reverse") == 0) {
+        } else if (strcmp(argument, "REVERSE") == 0) {
             reverse(array, length);
             printArr(array, length);
 
@@ -38,5 +39,6 @@ int main(int args, char* argv[]) {
             printf("Invalid argument.\n");
         }    
     }
+    free(array);
     return 1;
 }
---
+++ Worksheets and Lectures/Week04/functions.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int sum(int array[], int length) {
+    int i, answer = 0;
+    for (i = 0; i < length; i++) {
+        answer += array[i];
+    }
+    return answer;
+}
+
+int max(int array[], int length) {
+    int i, index = 0, highest = 0;
+    for (i = 0; i < length; i++) {
+        if (array[i] > highest) {
+            highest = array[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+void reverse(int array[], int length) {
+    int i, tmp = 0;
+    for (i = 0; i < length/2; i++) {
+        tmp = array[i];
+        array[i] = array[length - 1 - i];
+        array[length - 1 - i] = tmp;
+    }
+}
+
+void strToInt(char *charArray[], int intArray[], int length) {
+    int i;
+    for (i = 0; i < length; i++) {
+        intArray[i] = atoi(charArray[i]);
+    }
+}
+
+void printArr(int array[], int length) {
+    int i;
+    printf("{");
+    for (i = 0; i < length - 1; i++) {
+        printf("%d, ", array[i]);
+    }
+    printf("%d}\n", array[length - 1]);
+}
+
+void lowerUpper(char *array) {
+
+    /*Change type to equal: 1. Using strlen().
+                            2. Not using strlen().
+                            3. Not using array indexing. */
+    int type = 1;
+    int length;
+    int i;
+
+    switch(type) {
+        case 1:
+            length = strlen(array);
+            for (i = 0; i < length; i++) {
+                if ((array[i] > 96) && (array[i] < 123)) {
+                    array[i] = array[i] - 32;
+                }
+            }
+        break;
+        case 2:
+            i = 0;
+            while (array[i + 1] != '\0') {
+                if ((array[i] > 96) && (array[i] < 123)) {
+                    array[i] = array[i] - 32;
+                }
+                i += 1;
+            }
+
+        break;
+        case 3:
+
+        break;
+    }
+}
