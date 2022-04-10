b7ce697db1cc388954cb5b9b889e78a41cf7a331
Samuel-Wyatt <samuel.wyatt2@outlook.com>
2022-04-10 14:20:18 +0800

    Week05

--- Worksheets and Lectures/Week04/question2.h
+++
@@ -1,5 +0,0 @@
-int sum(int array[], int length);
-int reverse(int array[], int length);
-int max(int array[], int length);
-void strToInt(char *charArray[], int intArray[], int length);
-void printArr(int array[], int length);
---
+++ Worksheets and Lectures/Week05/worksheet 06/printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+    char *s1;
+    char *s2;
+    scanf("%s%s", s1, s2);
+    /*printf("%s %s", s1, s2);*/
+}
