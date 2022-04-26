c1b53afe55bb1e49f5a8d99776e0c520bc15b3e6
samuel-wyatt <samuel.wyatt2@outlook.com>
2022-04-26 21:45:30 +0800

    Testing on VM

--- Worksheets and Lectures/Week06/display.c
+++ Worksheets and Lectures/Week06/display.c
@@ -1,45 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "plot.h"
 int main(int args, char **argv) {
     FILE *fPtr;
     double **array, input;
-    int row, col, i, j;
+    int row, cols, i, j;
+    char *fileName;
     if (args < 2) {
         printf("USAGE: ./display <fileName>\n");
 
-    } else if ((fPtr = fopen(argv[1], "r")) == NULL) { 
-        printf("Unable to open file \"%s\"\n", argv[1]);
-
     } else {
-        fscanf(fPtr, "%d %d", &row, &col);
-        array = malloc(row * sizeof(double*));
+        fileName = argv[1];
+        if (strcmp(fileName, "-") != 0) {
+            if ((fPtr = fopen(fileName, "r")) == NULL) {
+                printf("Unable to open file \"%s\"\n", argv[1]);
+                return 1;
+            }
+        
+            fscanf(fPtr, "%d %d", &row, &cols);
+            
+            array = malloc(row * sizeof(double*));
+            for (i = 0; i < row; i++) {
+                array[i] = malloc(cols * sizeof(double));
+            }
 
-        for (i = 0; i < col; i++) {
-            array[i] = malloc(col * sizeof(double));
-        }
+            if (array == NULL) {
+                printf("Failed to allocate array\n");
 
-        if (array == NULL) {
-            printf("Failed to allocate array\n");
-
+            } else {
+                for (i = 0; i < row; i++) {
+                    for (j = 0; j < cols; j++) {
+                        fscanf(fPtr, "%lf ", &input);
+                        array[i][j] = input;
+                    }
+                }
+                plot(array, row, cols);
+            }
+        fclose(fPtr);
         } else {
 
-        for (i = 0; i < row; i++) {
-            for (j = 0; j < col; j++) {
-                fscanf(fPtr, "%lf ", &input);
-                array[i][j] = input;
+            fscanf(fPtr, "%d %d", &row, &cols);
+            array = malloc(row * sizeof(double*));
+
+            for (i = 0; i < cols; i++) {
+                array[i] = malloc(cols * sizeof(double));
+            }
+
+            if (array == NULL) {
+                printf("Failed to allocate array\n");
+
+            } else {
+
+            for (i = 0; i < row; i++) {
+                for (j = 0; j < cols; j++) {
+                    fscanf(stdin, "%lf", &input);
+                    array[i][j] = input;
+                }
+            }
+            plot(array, row, cols);
             }
         }
-        plot(array, row, col);
+        fPtr = NULL;
+        for (i = 0; i < row; i++) {
+            free(array[i]);
         }
-    fclose(fPtr);
-    fPtr = NULL;
-
-    for (i = 0; i < row; i++) {
-        free(array[i]);
-    }
-    free(array);
-    array = NULL;
+        free(array);
+        array = NULL;
     }
     return 0;
 }
---
+++ Worksheets and Lectures/Week06/generate.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "randomarray.h"
+int main(int argc, char **argv) {
+    FILE *fPtr;
+    int rows, cols, i, j;
+    double **array;
+    char *fileName;
+
+    fileName = argv[1];
+    rows = atoi(argv[2]);
+    cols = atoi(argv[3]);
+
+    if (argc < 4) {
+        printf("Usage : ./generate <filename> <rows> <columns>\n");
+    } else {
+        if (strcmp(fileName, "-") != 0) {
+            if ((fPtr = fopen(fileName, "w")) == NULL) {
+                printf("Error : Could not open file. Aborting\n");
+                return 1;
+            }
+
+            array = (double**)malloc(rows * sizeof(double*));
+            for (i = 0; i < rows; i++) {
+                array[i] = (double*)malloc(cols * sizeof(double));
+            }
+            if (array == NULL) {
+                printf("Failed to allocate array. Aborting\n");
+                return 1;
+            }
+        
+            randomArray(array, rows, cols, 0);
+            
+            fprintf(fPtr, "%d %d\n", rows, cols);
+            for (i = 0; i < rows; i++) {
+                for (j = 0; j < cols; j++) {
+                    fprintf(fPtr, "%f ", array[i][j]);
+                }
+                    fprintf(fPtr, "\n");
+            }
+
+            fclose(fPtr);
+        } else {
+            array = (double**)malloc(rows * sizeof(double*));
+            for (i = 0; i < rows; i++) {
+                array[i] = (double*)malloc(cols * sizeof(double));
+            }
+            if (array == NULL) {
+                printf("Failed to allocate array. Aborting\n");
+                return 1;
+            }
+        
+            randomArray(array, rows, cols, 0);
+            
+            fprintf(stdout, "%d %d\n", rows, cols);
+            for (i = 0; i < rows; i++) {
+                for (j = 0; j < cols; j++) {
+                    fprintf(stdout, "%f ", array[i][j]);
+                }
+                    fprintf(stdout, "\n");
+            }
+        }
+
+            for (i = 0; i < rows; i++) {
+                free(array[i]);
+            }
+            free(array);
+            array = NULL;
+            fPtr = NULL;
+
+    }
+    return 0;
+}
